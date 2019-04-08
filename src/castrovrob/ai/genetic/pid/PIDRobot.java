4a6c9de675f9cc31264afa03f771395dc2d45d1a
Roberto Castro Valenzuela <roberto.castrovalenzuela@ucr.ac.cr>
2019-04-07 23:39:02 -0600

    Initial commit
    Remake of my previous .Net project

---
+++ src/castrovrob/ai/genetic/pid/PIDRobot.java
@@ -0,0 +1,85 @@
+package castrovrob.ai.genetic.pid;
+
+import castrovrob.dyn4j.framework.SimulationBody;
+import org.dyn4j.dynamics.World;
+import org.dyn4j.dynamics.joint.RevoluteJoint;
+import org.dyn4j.geometry.Geometry;
+import org.dyn4j.geometry.MassType;
+import org.dyn4j.geometry.Vector2;
+
+public class PIDRobot extends SimulationBody implements Runnable{
+    private boolean isRunning = true;
+
+    private SimulationBody circle;
+    private SimulationBody stick;
+    private RevoluteJoint circleToStick;
+
+    private double previousError = 0.0;
+
+    private final int PROPORTIONAL_CONST = 5;
+    private final int INTEGRAL_CONST = 1;
+    private final int DERIVATIVE_CONST = 5;
+
+    public PIDRobot(World mWorld){
+        circle = new SimulationBody();
+        circle.addFixture(Geometry.createCircle(1.0));
+        circle.setMass(MassType.NORMAL);
+        circle.translate(0.0,0.0);
+
+        stick = new SimulationBody();
+        stick.addFixture(Geometry.createRectangle(0.1, 3.75));
+        stick.setMass(MassType.NORMAL);
+        stick.translate(0.0, 1.875);
+
+        circleToStick = new RevoluteJoint(circle, stick, new Vector2(0.0, 0.0));
+        circleToStick.setLimitEnabled(false);
+        circleToStick.setLimits(Math.toRadians(0.0), Math.toRadians(0.0));
+        circleToStick.setReferenceAngle(Math.toRadians(0.0));
+        circleToStick.setMotorEnabled(false);
+        circleToStick.setMotorSpeed(Math.toRadians(0.0));
+        circleToStick.setMaximumMotorTorque(0.0);
+
+        circleToStick.setCollisionAllowed(true);
+
+        mWorld.addBody(circle);
+        mWorld.addBody(stick);
+        mWorld.addJoint(circleToStick);
+    }
+
+    public void PIDControl(){
+        double offset = 0.0;
+        double normalValue = Math.toDegrees(stick.getRotationDiscRadius());
+
+        double proportionalError = 0.0;
+        proportionalError = normalValue - offset;
+
+        double integralError = 0.0;
+        integralError = ((integralError + proportionalError) * 2 / 3);
+
+        double derivativeError = 0.0;
+        derivativeError = proportionalError - this.previousError;
+
+        double pidValue = PROPORTIONAL_CONST * proportionalError + INTEGRAL_CONST * integralError + DERIVATIVE_CONST * derivativeError;
+        this.previousError = proportionalError;
+
+        this.circleToStick.setMaximumMotorTorque(pidValue);
+        //this.circleToStick.setMotorSpeed(pidValue);
+    }
+
+    public void disposeRobot(){
+        this.isRunning = false;
+    }
+
+    @Override
+    public void run(){
+        try {
+            while (isRunning) {
+                this.PIDControl();
+                Thread.sleep(200);
+            }
+        }catch(InterruptedException interruptedException){
+            interruptedException.printStackTrace();
+        }
+    }
+
+}
---
+++ src/castrovrob/ai/genetic/pid/RobotParty.java
@@ -0,0 +1,42 @@
+package castrovrob.ai.genetic.pid;
+
+import castrovrob.dyn4j.framework.SimulationBody;
+import castrovrob.dyn4j.framework.SimulationFrame;
+import org.dyn4j.dynamics.World;
+import org.dyn4j.geometry.Geometry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RobotParty extends SimulationFrame{
+
+    private List<PIDRobot> myRobots;
+
+    protected void initializeWorld(){
+        this.world.setGravity(World.EARTH_GRAVITY);
+        this.createGround();
+    }
+
+    private RobotParty(){
+        super("Genetic Algorithm PID", 40.0);
+    }
+
+    private void createGround(){
+        SimulationBody ground = new SimulationBody();
+        ground.addFixture(Geometry.createRectangle(15.0, 1.0));
+        ground.translate(0.0, -3.5);
+        this.world.addBody(ground);
+    }
+
+    private void createRobots(){
+        this.myRobots = new ArrayList<>();
+        this.myRobots.add(new PIDRobot(this.world));
+        this.myRobots.get(0).run();
+    }
+
+    public static void main(String[] args){
+        RobotParty simulation = new RobotParty();
+        simulation.run();
+        simulation.createRobots();
+    }
+}
